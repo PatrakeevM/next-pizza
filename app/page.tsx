bd64cc91314d4a863c64547524ca06a16bd96ccf
PatrakeevM <patrakeev.matvej@bk.ru>
2024-10-05 21:28:50 +0300

    Разработал Header. Добавил базовые UI-компоненты

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import { Button } from "@/components/ui/button";
+import { Container, Filter, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
-    <h1>Home
-      <Button variant={"outline"}>Корзина</Button>
-    </h1>
+    <>
+      <Container className="mt-5">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          <Filter />
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Список товаров</div>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
